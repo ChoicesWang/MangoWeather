e68758df85101051d0c3a5ea00a4eba69fde834f
Choices <zezeviyao@aliyun.com>
2016-03-18 16:43:34 +0800

    适配 status bar

--- app/src/main/java/com/choices/weather/MainActivity.java
+++ app/src/main/java/com/choices/weather/MainActivity.java
@@ -1,8 +1,11 @@
 package com.choices.weather;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.choices.weather.widget.HeaderLayout;
 
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
